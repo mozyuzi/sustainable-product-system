39674a6a47d7e075436916d81d0e5d118ebadef3
itsBelal <116500420+itsBelal@users.noreply.github.com>
2026-04-27 23:20:53 +0200

    Create ConsoleUI for product management system
    Added ConsoleUI class for user interface.

--- src/main/java/se/sustainableproductsystem/presentation/ConsoleUI.java
+++ src/main/java/se/sustainableproductsystem/presentation/ConsoleUI.java
@@ -1,1 +1,15 @@
+package se.sustainableproductsystem.presentation;
 
+import se.sustainableproductsystem.application.ProductService;
+
+public class ConsoleUI {
+    private ProductService productService;
+
+    public ConsoleUI(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public void start() {
+        System.out.println("Sustainable Product & Recycling Management System");
+    }
+}
