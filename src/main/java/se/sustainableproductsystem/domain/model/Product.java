8a503f5b15d998d4ee7e5131a7831f6e9341259e
itsBelal <116500420+itsBelal@users.noreply.github.com>
2026-04-27 23:16:45 +0200

    Add Product class with fields for name, category, and materials

--- src/main/java/se/sustainableproductsystem/domain/model/Product.java
+++ src/main/java/se/sustainableproductsystem/domain/model/Product.java
@@ -1,1 +1,10 @@
+package se.sustainableproductsystem.domain.model;
 
+import java.util.List;
+
+public class Product {
+    private String name;
+    private Category category;
+    private Lifespan lifespan;
+    private List<Material> materials;
+}
