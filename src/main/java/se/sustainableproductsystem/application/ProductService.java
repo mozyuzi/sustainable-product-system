d4dd7227b8da428e75e2c176b9cc33cb2a63dde1
mozyuzi <mozyuzii@gmail.com>
2026-04-27 23:33:16 +0200

    Enhanced ProductService

--- src/main/java/se/sustainableproductsystem/application/ProductService.java
+++ src/main/java/se/sustainableproductsystem/application/ProductService.java
@@ -1,22 +1,43 @@
 package se.sustainableproductsystem.application;
 
+import java.util.ArrayList;
+import java.util.List;
 import se.sustainableproductsystem.domain.model.Product;
 import se.sustainableproductsystem.domain.model.RecyclingGuidance;
 import se.sustainableproductsystem.domain.strategy.ImpactCalculationStrategy;
 
 public class ProductService {
-    private ImpactCalculationStrategy impactCalculationStrategy;
+    private final ImpactCalculationStrategy impactCalculationStrategy;
+    private final List<Product> products = new ArrayList<>();
 
     public ProductService(ImpactCalculationStrategy impactCalculationStrategy) {
         this.impactCalculationStrategy = impactCalculationStrategy;
+    }
+
+    public void createProduct(String name) {
+        products.add(new Product(name));
+    }
+
+    public List<Product> getProducts() {
+        return products;
     }
 
     public double calculateImpact(Product product) {
         return impactCalculationStrategy.calculateImpact(product);
     }
 
+    public double calculateImpactByName(String name) {
+        for (Product product : products) {
+            if (product.getName().equalsIgnoreCase(name)) {
+                return calculateImpact(product);
+            }
+        }
+        return 0;
+    }
+
     public RecyclingGuidance getRecyclingGuidance(Product product) {
         return new RecyclingGuidance();
     }
 }
+
 
