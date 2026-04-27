8ed20c85a3df685f14484851bade55508c04de2a
Bashar Ghaleb Kharfan <thematrixt68@gmail.com>
2026-04-27 14:25:38 -0700

    Implement SimpleImpactCalculation class

--- src/main/java/se/sustainableproductsystem/domain/strategy/SimpleImpactCalculation.java
+++ src/main/java/se/sustainableproductsystem/domain/strategy/SimpleImpactCalculation.java
@@ -1,1 +1,10 @@
+package se.sustainableproductsystem.domain.strategy;
 
+import se.sustainableproductsystem.domain.model.Product;
+
+public class SimpleImpactCalculation implements ImpactCalculationStrategy {
+    @Override
+    public double calculateImpact(Product product) {
+        return 0;
+    }
+}
