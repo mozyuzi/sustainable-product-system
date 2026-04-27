6bcc00b9504ca87d94ddba0c62c7e90c1832c3ba
mozyuzi <mozyuzii@gmail.com>
2026-04-27 23:18:37 +0200

    updated AdvancedImpactCalculation

--- src/main/java/se/sustainableproductsystem/domain/strategy/AdvancedImpactCalculation.java
+++ src/main/java/se/sustainableproductsystem/domain/strategy/AdvancedImpactCalculation.java
@@ -1,1 +1,10 @@
+package se.sustainableproductsystem.domain.strategy;
 
+import se.sustainableproductsystem.domain.model.Product;
+
+public class AdvancedImpactCalculation implements ImpactCalculationStrategy {
+    @Override
+    public double calculateImpact(Product product) {
+        return 0;
+    }
+}
