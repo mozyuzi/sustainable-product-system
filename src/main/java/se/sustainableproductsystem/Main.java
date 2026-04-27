65c56a20c7203baf68fb04c28df195027c6699ee
itsBelal <116500420+itsBelal@users.noreply.github.com>
2026-04-27 23:38:13 +0200

    Remove unused Product class from Main.java
    Removed unused Product class and related imports.

--- src/main/java/se/sustainableproductsystem/Main.java
+++ src/main/java/se/sustainableproductsystem/Main.java
@@ -11,21 +11,3 @@ public class Main {
         consoleUI.start();
     }
 }
-package se.sustainableproductsystem.domain.model;
-
-import java.util.List;
-
-public class Product {
-    private String name;
-    private Category category;
-    private Lifespan lifespan;
-    private List<Material> materials;
-
-    public Product(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-}
