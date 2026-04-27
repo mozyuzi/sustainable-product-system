921a1ce890699a562d3b06f830a3ae3079a0820a
itsBelal <116500420+itsBelal@users.noreply.github.com>
2026-04-27 23:17:08 +0200

    Add Material class with properties for sustainability

--- src/main/java/se/sustainableproductsystem/domain/model/Material.java
+++ src/main/java/se/sustainableproductsystem/domain/model/Material.java
@@ -1,1 +1,8 @@
+package se.sustainableproductsystem.domain.model;
 
+public class Material {
+    private String name;
+    private double impactValue;
+    private RecyclingCategory recyclingCategory;
+    private String recyclingInstruction;
+}
