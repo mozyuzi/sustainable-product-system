c5b8b27f3be27acc2be291f5875f84d963570447
mozyuzi <mozyuzii@gmail.com>
2026-04-27 23:21:36 +0200

    updated productService

--- src/main/java/se/sustainableproductsystem/application/ProductService.java
+++ src/main/java/se/sustainableproductsystem/application/ProductService.java
@@ -1,1 +1,22 @@
+package se.sustainableproductsystem.application;
+
+import se.sustainableproductsystem.domain.model.Product;
+import se.sustainableproductsystem.domain.model.RecyclingGuidance;
+import se.sustainableproductsystem.domain.strategy.ImpactCalculationStrategy;
+
+public class ProductService {
+    private ImpactCalculationStrategy impactCalculationStrategy;
+
+    public ProductService(ImpactCalculationStrategy impactCalculationStrategy) {
+        this.impactCalculationStrategy = impactCalculationStrategy;
+    }
+
+    public double calculateImpact(Product product) {
+        return impactCalculationStrategy.calculateImpact(product);
+    }
+
+    public RecyclingGuidance getRecyclingGuidance(Product product) {
+        return new RecyclingGuidance();
+    }
+}
 
