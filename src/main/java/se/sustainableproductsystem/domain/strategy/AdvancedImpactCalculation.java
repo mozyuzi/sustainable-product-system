bb7c1eb1c5ace78c09c1474a7b96f0a721203a6c
itsBelal <116500420+itsBelal@users.noreply.github.com>
2026-05-05 03:30:26 +0200

    Implement impact calculation for products with materials

--- src/main/java/se/sustainableproductsystem/domain/strategy/AdvancedImpactCalculation.java
+++ src/main/java/se/sustainableproductsystem/domain/strategy/AdvancedImpactCalculation.java
@@ -1,10 +1,22 @@
 package se.sustainableproductsystem.domain.strategy;
 
+import se.sustainableproductsystem.domain.model.Material;
 import se.sustainableproductsystem.domain.model.Product;
 
 public class AdvancedImpactCalculation implements ImpactCalculationStrategy {
+    private static final double MIXED_MATERIAL_MULTIPLIER = 1.2;
+
     @Override
     public double calculateImpact(Product product) {
-        return 0;
+        double baseImpact = 0;
+        for (Material material : product.getMaterials()) {
+            baseImpact += material.getImpactValue();
+        }
+
+        if (product.getMaterials().size() > 1) {
+            return baseImpact * MIXED_MATERIAL_MULTIPLIER;
+        }
+
+        return baseImpact;
     }
 }
