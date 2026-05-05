f6b9b1bf9624c031e2afcd5d6ee619180adb4c4f
itsBelal <116500420+itsBelal@users.noreply.github.com>
2026-05-05 03:35:19 +0200

    Implement impact calculation in SimpleImpactCalculation

--- src/main/java/se/sustainableproductsystem/domain/strategy/SimpleImpactCalculation.java
+++ src/main/java/se/sustainableproductsystem/domain/strategy/SimpleImpactCalculation.java
@@ -1,10 +1,15 @@
 package se.sustainableproductsystem.domain.strategy;
 
+import se.sustainableproductsystem.domain.model.Material;
 import se.sustainableproductsystem.domain.model.Product;
 
 public class SimpleImpactCalculation implements ImpactCalculationStrategy {
     @Override
     public double calculateImpact(Product product) {
-        return 0;
+        double totalImpact = 0;
+        for (Material material : product.getMaterials()) {
+            totalImpact += material.getImpactValue();
+        }
+        return totalImpact;
     }
 }
